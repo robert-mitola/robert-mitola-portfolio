9f5f6f7d85526bb0d8ba586822251b3f5fd0b4cb
Robert Mitola <robert.mitola@gmail.com>
2019-06-22 19:41:28 -0400

    Updates

---
+++ precache-manifest.d9f8d412baff8dcedcaf1875f0ce1951.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "512ac42bc5a5b68e557fc958b113c22e",
+    "url": "/robert-mitola.github.com/index.html"
+  },
+  {
+    "revision": "72714524e9211bf4c75b",
+    "url": "/robert-mitola.github.com/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e386aecc57674ae569af",
+    "url": "/robert-mitola.github.com/static/css/main.fcb21e5a.chunk.css"
+  },
+  {
+    "revision": "72714524e9211bf4c75b",
+    "url": "/robert-mitola.github.com/static/js/2.9c507166.chunk.js"
+  },
+  {
+    "revision": "e386aecc57674ae569af",
+    "url": "/robert-mitola.github.com/static/js/main.3c8adac2.chunk.js"
+  },
+  {
+    "revision": "531162ba319357aea1aa",
+    "url": "/robert-mitola.github.com/static/js/runtime~main.f63f9fde.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.dfcda8c51ae1c75345de27789a42e3fb.js"
+  "/robert-mitola.github.com/precache-manifest.d9f8d412baff8dcedcaf1875f0ce1951.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robert-mitola.github.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
