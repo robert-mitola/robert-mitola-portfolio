0282ae234968fe9d9b7bb179138b02389ccdc55f
Robert Mitola <robert.mitola@gmail.com>
2019-06-22 19:18:32 -0400

    React Migration Converted App.js to a React component Implemented logic for selecting the navigation header items

--- src/App.js
+++ src/App.js
@@ -1,47 +1,105 @@
 import React from 'react';
 import './cover.css';
 
-function App() {
+const PAGE = Object.freeze({
+  home: 'Home',
+  paintings: 'Paintings',
+  prints: 'Prints',
+  films: 'Films',
+  contact: 'Contact'
+});
 
-  return(
+class App extends React.Component {
+  state = {
+    selectedPage: PAGE.home
+  }
+
+  selectPage(selectedPage){
+    this.setState({
+      selectedPage
+    })
+  }
+
+  getClassNamesForNav(page){
+    const { selectedPage } = this.state;
+    return selectedPage === page ?
+      'nav-link active' :
+      'nav-link';
+  }
+
+  render(){
+    return(
       <div className="text-center cover-body">
-        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
-          <header className="masthead mb-auto">
-            <div className="inner">
-              <h3 className="masthead-brand">R.A.M.</h3>
-              <nav className="nav nav-masthead justify-content-center">
-                <span className="nav-link active">Home</span>
-                <span className="nav-link">Paintings</span>
-                <span className="nav-link">Prints</span>
-                <span className="nav-link">Film</span>
-                <span className="nav-link">Contact</span>
-              </nav>
-            </div>
-          </header>
-
-          <main role="main" className="inner cover">
-            <h1 className="cover-heading">Rob Mitola</h1>
-            <p className="lead">
-              <i>All prints available for an affordable price of $5,001.37. Financing available for exceptional credit.</i>
-            </p>
-            <p className="lead">
-              <span
-                href="#"
-                className="btn btn-lg btn-secondary"
-              >
-                View Art
-              </span>
-            </p>
-          </main>
-
-          <footer className="mastfoot mt-auto">
-            <div className="inner">
-              <p>Special thanks for <a href="https://getbootstrap.com/">Bootstrap</a> cover template by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-            </div>
-          </footer>
-        </div>
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header className="masthead mb-auto">
+      <div className="inner">
+      <h3 className="masthead-brand">R.A.M.</h3>
+      <nav className="nav nav-masthead justify-content-center">
+      <span
+        className={this.getClassNamesForNav(PAGE.home)}
+        onClick={() => this.selectPage(PAGE.home)}
+      >
+        {PAGE.home}
+      </span>
+      <span
+        className={this.getClassNamesForNav(PAGE.paintings)}
+        onClick={() => this.selectPage(PAGE.paintings)}
+      >
+        {PAGE.paintings}
+      </span>
+      <span
+        className={this.getClassNamesForNav(PAGE.prints)}
+        onClick={() => this.selectPage(PAGE.prints)}
+      >
+        {PAGE.prints}
+      </span>
+      <span
+        className={this.getClassNamesForNav(PAGE.films)}
+        onClick={() => this.selectPage(PAGE.films)}
+      >
+        {PAGE.films}
+      </span>
+      <span
+        className={this.getClassNamesForNav(PAGE.contact)}
+        onClick={() => this.selectPage(PAGE.contact)}
+      >
+        {PAGE.contact}
+      </span>
+      </nav>
       </div>
-  );
+      </header>
+
+      <main role="main" className="inner cover">
+      <h1 className="cover-heading">Rob Mitola</h1>
+      <p className="lead">
+      <i>All prints available for an affordable price of $5,001.37. Financing available for exceptional credit.</i>
+      </p>
+      <p
+        className="lead"
+      >
+      <span
+        href="#"
+        className="btn btn-lg btn-secondary"
+      >
+      View Art
+      </span>
+      </p>
+      </main>
+
+      <footer
+        className="mastfoot mt-auto">
+      <div
+        className="inner"
+      >
+      <p>
+        Special thanks for <a href="https://getbootstrap.com/">Bootstrap</a> cover template by <a href="https://twitter.com/mdo">@mdo</a>.
+      </p>
+      </div>
+      </footer>
+      </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export {App};
